a9b8c5b1a3217b5925e3d44d3e1b63e1d90057dc
Yandiel Lillo <lillo.yandiel@seiza-ti.cl>
2023-06-01 13:49:17 -0400

    [Clase 89] Servir contenido estático

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const http = require("http");
+const express = require('express')
+const app = express()
 
-http.createServer( (req, res) => {
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
-    res.writeHead(200, {
-        //'Content-Type': 'text/plain'
-        'Content-Type': 'application/csv'
-    })
-    res.write("work!");
-    res.end();
-}).listen(8080);
+const port = 8080;
 
-console.log("Escuchando en el puerto 8080");
+
+// Servir contenido estático
+app.use( express.static('public'));
+// consideracion, debe existir el archivo index.html, si no dara error
+
+// Si agrego una carpeta en la ruta de public, ejemplo /public/hola-mundo/index.html
+// tendrá prioridad antes que un app.get('/hola-mundo')
+
+app.get('/about', function (req, res) {
+    res.send('work!')
+  })
+
+  // Para cualquier ruta no mapeada
+  app.get('*', (req, res) => {
+    //res.send('404 Page not found!')
+    res.sendFile( __dirname + '/public/404.html');
+  })
+
+//app.listen(8080)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
---
+++ app.old.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+http.createServer( (req, res) => {
+    //res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    //res.writeHead(200, {
+        //'Content-Type': 'text/plain'
+        //'Content-Type': 'application/csv'
+    //})
+    res.write("work!");
+    res.end();
+}).listen(8080);
+
+console.log("Escuchando en el puerto 8080");
